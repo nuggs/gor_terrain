ff0c71db1aaf0c70d8cb3844821ac9c973f5b074
Anthony Goins <the.green.machine@gmail.com>
2021-05-10 14:32:24 -0700

    Ported multi_texture to C++.  Hopefully

---
+++ multi_texture.cpp
@@ -0,0 +1,126 @@
+
+
+#include "multi_texture.h"
+
+void multi_texture::_set_data(const Dictionary &data) {
+    _set_data(data);
+    emit_signal("changed");
+    property_list_changed_notify();
+}
+
+void multi_texture::set_selected(int _selected) {
+    selected = _selected;
+    emit_signal("changed");
+}
+
+const int multi_texture::get_selected(void) {
+    return selected;
+}
+
+const Vector2 multi_texture::get_size(void) {
+    return Vector2(get_width(), get_height());
+}
+
+const Dictionary multi_texture::create_data(int width, int height, int depth, int format, int flags = 7) {
+    Dictionary data = Dictionary();
+
+    data["width"] = width;
+    data["height"] = height;
+    data["depth"] = depth;
+    data["format"] = format;
+    data["flags"] = flags;
+    data["layers"] = Array();
+
+    return data;
+}
+
+void multi_texture::set_layer(Ref<ImageTexture> src, int idx, int chn_src, int chn_dst) {
+    Ref<Image> img_src = Ref<Image>();
+    Ref<Image> dst = Ref<Image>();
+    Vector2 size = get_size();
+
+    img_src = src->get_data();
+
+    if (img_src->get_size() != size)
+        img_src->resize(get_width(), get_height());
+
+    if (img_src->is_compressed())
+        img_src->decompress();
+
+    if (img_src->get_format() != get_format())
+        img_src->convert(get_format());
+
+    if (chn_src < 0 || chn_dst < 0) {
+        dst = img_src;
+     } else {
+        dst = get_layer_data(idx);
+        img_src->lock();
+        dst->lock();
+
+        for (int y = 0; y <= size.y; y++) {
+            for (int x = 0; x < size.x; x++) {
+                Color clr = dst->get_pixel(x, y);
+                clr[chn_dst] = img_src->get_pixel(x, y)[chn_src];
+                dst->set_pixel(x, y, clr);
+            }
+        }
+
+        dst->unlock();
+        img_src->unlock();
+    }
+
+    dst->generate_mipmaps();
+    set_layer_data(dst, idx);
+    emit_signal("changed");
+    property_list_changed_notify();
+}
+
+void multi_texture::set_layer(Ref<Image> src, int idx, int chn_src, int chn_dst) {
+    Ref<Image> dst = Ref<Image>();
+    Vector2 size = get_size();
+
+    if (src->get_size() != size)
+        src->resize(get_width(), get_height());
+
+    if (src->is_compressed())
+        src->decompress();
+
+    if (src->get_format() != get_format())
+        src->convert(get_format());
+
+    if (chn_src < 0 || chn_dst < 0) {
+        dst = src;
+    } else {
+        dst = get_layer_data(idx);
+        src->lock();
+        dst->lock();
+
+        for (int y = 0; y <= size.y; y++) {
+            for (int x = 0; x <= size.x; x++) {
+                Color clr = dst->get_pixel(x, y);
+                clr[chn_dst] = src->get_pixel(x, y)[chn_src];
+                dst->set_pixel(x, y, clr);
+            }
+        }
+
+        src->unlock();
+        dst->unlock();
+    }
+
+    dst->generate_mipmaps();
+    set_layer_data(dst, idx);
+    emit_signal("changed");
+    property_list_changed_notify();
+}
+
+Ref<Image> multi_texture::get_layer(int idx) {
+    Array layers;
+    for (unsigned int i = 0; i < get_depth(); i++) {
+        layers.push_back(get_layer_data(i));
+    }
+
+    if (idx < layers.size())
+            return layers[idx];
+    return nullptr;
+}
+
---
+++ multi_texture.h
@@ -0,0 +1,27 @@
+
+#ifndef MULTI_TEXTURE_H
+#define MULTI_TEXTURE_H
+
+#include <core/image.h>
+#include <scene/resources/texture.h>
+
+class multi_texture : public TextureArray {
+    GDCLASS(multi_texture, TextureArray);
+
+    public:
+        void set_selected(int _selected);
+        const int get_selected(void);
+        const Vector2 get_size(void);
+        const Dictionary create_data(int width, int height, int depth, int format, int flags);
+        void set_layer(Ref<ImageTexture> src, int idx, int chn_src, int chn_dst);
+        void set_layer(Ref<Image> src, int idx, int chn_src, int chn_dst);
+        Ref<Image> get_layer(int idx);
+
+    protected:
+        void _set_data(const Dictionary &data);
+
+    private:
+        int selected = -1;
+};
+
+#endif // MULTI_TEXTURE_H
